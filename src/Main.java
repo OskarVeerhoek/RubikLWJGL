d9e7262bdf10f5caef141b742d0ac934ab66e17e
Oskar Veerhoek <support@thecodinguniverse.com>
2012-10-27 10:42:07 +0200

    added anti-aliasing

--- src/Main.java
+++ src/Main.java
@@ -11,27 +11,36 @@ public class Main {
 		Display.setTitle("A Rubix Cube in LWJGL - Oskar");
 		try {
 			Display.setDisplayMode(new DisplayMode(640, 480));
-			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 			Display.destroy();
 			System.exit(1);
 		}
+		try {
+			Display.create(new PixelFormat(32, 0, 24, 8, 4));
+		} catch (LWJGLException e) {
+			System.err.println("Failed to create multisampled OpenGL context. Creating normal one.");
+			try {
+				Display.create();
+			} catch (LWJGLException ex) {
+				ex.printStackTrace();
+				Display.destroy();
+				System.exit(1);
+			}
+		}
 		RubixCamera camera = new RubixCamera();
 		camera.initialise();
-		RubixCube cube = new RubixCube(3);
-		cube.initialise();
-//		RubixBlock block = new RubixBlock(RubixBlock.Side.WHITE,RubixBlock.Side.YELLOW,RubixBlock.Side.BLUE,RubixBlock.Side.GREEN,RubixBlock.Side.RED,RubixBlock.Side.ORANGE);
-//		block.initialise();
+		//RubixCube cube = new RubixCube(3);
+		//cube.initialise();
+		RubixBlock block = new RubixBlock(RubixBlock.Side.WHITE,RubixBlock.Side.YELLOW,RubixBlock.Side.BLUE,RubixBlock.Side.GREEN,RubixBlock.Side.RED,RubixBlock.Side.ORANGE);
+		block.initialise();
 		boolean running = true;
 		while (running) {
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			camera.translate();
 			camera.handleInput();
-			System.out.println(gluErrorString(glGetError()));
-			cube.draw();
-			System.out.println(gluErrorString(glGetError()));
-			//			block.draw();
+			//cube.draw();
+			block.draw();
 			running = (Display.isCloseRequested()) ? false : running;
 			if (Keyboard.isKeyDown(Keyboard.KEY_RETURN)) {
 				running = false;
